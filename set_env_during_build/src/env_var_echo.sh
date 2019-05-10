028a7404f3b9dbb9a61cead0aa409bf50f767725
morne <morne.kruger@gmail.com>
2019-05-10 22:24:44 +0200

    alpha

---
+++ set_env_during_build/src/env_var_echo.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+echo Welcome to the environment variable example!
+echo Checking value for '$LOGNAME' value = $LOGNAME
+echo This shell script will echo the environment variable called:my_env_var
+echo Value of "my_env_var" = $my_env_var
+echo "The script will sleep for 2 minutes - starting now"
+sleep 2m
